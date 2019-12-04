2909a87982ce0b16d444b64d9a9a57b8cf853fad
ShujunHan <44661662+ShujunHan@users.noreply.github.com>
2019-12-04 11:25:37 +0800

    添加单利模式

--- DesignsPatternProject/Program.cs
+++ DesignsPatternProject/Program.cs
@@ -6,28 +6,28 @@ namespace DesignsPatternProject
     {
 
         #region Builder Pattern
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
 
-            Director director = new Director();//创建指挥者
+        //    Director director = new Director();//创建指挥者
 
-            Builder builderA = new ConcreteBuilderA();//创建具体执行者A
+        //    Builder builderA = new ConcreteBuilderA();//创建具体执行者A
 
-            Builder builderB = new ConcreateBuilderB();//创建具体执行者B
-
-
-            director.Build(builderA);//给具体执行者A赋值，让里面的方法执行
-            Product productA = builderA.GetRestult();//给产品角色赋值   
-            productA.Show();//显示出来
+        //    Builder builderB = new ConcreateBuilderB();//创建具体执行者B
 
 
-            director.Build(builderB);
-            Product productB = builderB.GetRestult();
-            productB.Show();
-            Console.ReadKey();
+        //    director.Build(builderA);//给具体执行者A赋值，让里面的方法执行
+        //    Product productA = builderA.GetRestult();//给产品角色赋值   
+        //    productA.Show();//显示出来
 
-        }
+
+        //    director.Build(builderB);
+        //    Product productB = builderB.GetRestult();
+        //    productB.Show();
+        //    Console.ReadKey();
+
+        //}
         #endregion
 
         //static void Main(string[] args)
